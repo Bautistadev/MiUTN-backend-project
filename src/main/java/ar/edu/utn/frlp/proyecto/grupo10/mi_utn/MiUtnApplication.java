16c7cf7f778f173ee503fb46de02841a5d7733b5
Mariano Ezequiel Serrano <marianoeserrano15@gmail.com>
2025-08-28 10:58:49 -0300

    Initial commit

---
+++ src/main/java/ar/edu/utn/frlp/proyecto/grupo10/mi_utn/MiUtnApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frlp.proyecto.grupo10.mi_utn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiUtnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiUtnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/frlp/proyecto/grupo10/mi_utn/MiUtnApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frlp.proyecto.grupo10.mi_utn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiUtnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
