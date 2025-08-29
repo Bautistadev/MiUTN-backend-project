93203847c9b36e819b44950971ebdeeebd85f3e1
Bautista <BautistaBasilioDev@outlook.com>
2025-08-29 15:10:49 -0300

    feat: mala practica para la creacion de los packages

---
+++ src/main/java/ar/edu/utn/frlp/proyecto/grupo10/mi_utn/model/example.java
@@ -0,0 +1,4 @@
+package ar.edu.utn.frlp.proyecto.grupo10.mi_utn.model;
+
+public class example {
+}
---
+++ src/main/java/ar/edu/utn/frlp/proyecto/grupo10/mi_utn/utils/example.java
@@ -0,0 +1,4 @@
+package ar.edu.utn.frlp.proyecto.grupo10.mi_utn.utils;
+
+public class example {
+}
