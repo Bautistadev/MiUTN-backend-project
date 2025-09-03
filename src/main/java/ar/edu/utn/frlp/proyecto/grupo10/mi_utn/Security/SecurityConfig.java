d3e53d0e4194ab52c16d1b4a9148bce1099ec18c
Bautista <BautistaBasilioDev@outlook.com>
2025-09-03 01:49:18 -0300

    feat: implementacion de spring security y deshabilitacion de cors, rompen las bolas siempre

--- src/main/java/ar/edu/utn/frlp/proyecto/grupo10/mi_utn/Security/SecurityConfig.java
+++ src/main/java/ar/edu/utn/frlp/proyecto/grupo10/mi_utn/Security/SecurityConfig.java
@@ -1,4 +1,23 @@
 package ar.edu.utn.frlp.proyecto.grupo10.mi_utn.Security;
 
+import jakarta.servlet.FilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(e->e.anyRequest().permitAll())
+                .build();
+    }
 }
