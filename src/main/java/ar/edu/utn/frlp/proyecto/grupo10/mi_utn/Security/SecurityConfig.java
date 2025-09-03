530c0a177c8ff9fccd37f9468b01c75d55073185
Bautista <BautistaBasilioDev@outlook.com>
2025-09-03 14:55:33 -0300

    feat:deshabilitar cors

--- src/main/java/ar/edu/utn/frlp/proyecto/grupo10/mi_utn/Security/SecurityConfig.java
+++ src/main/java/ar/edu/utn/frlp/proyecto/grupo10/mi_utn/Security/SecurityConfig.java
@@ -1,12 +1,21 @@
 package ar.edu.utn.frlp.proyecto.grupo10.mi_utn.Security;
 
+
+import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -20,4 +29,18 @@ public class SecurityConfig {
                 .authorizeHttpRequests(e->e.anyRequest().permitAll())
                 .build();
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // o limitar a tu dominio/ngrok
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
 }
