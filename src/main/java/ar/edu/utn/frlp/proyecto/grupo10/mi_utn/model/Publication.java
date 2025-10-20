7b0c857d3d70cd3ae117b365cfbb66b53071e478
Bautista <BautistaBasilioDev@outlook.com>
2025-10-19 23:31:09 -0300

    cambio de nombre de tabla, solo la p uwu

--- src/main/java/ar/edu/utn/frlp/proyecto/grupo10/mi_utn/model/Publication.java
+++ src/main/java/ar/edu/utn/frlp/proyecto/grupo10/mi_utn/model/Publication.java
@@ -14,7 +14,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "Publications")
+@Table(name = "publications")
 @Builder
 @Data
 @AllArgsConstructor
